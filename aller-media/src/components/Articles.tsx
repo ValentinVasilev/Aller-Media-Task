96940ec6f4ac823eb4be1858ca8bedc93f6bfa61
ValentinVasilev <falioVasilev@gmail.com>
2022-12-03 14:06:56 +0200

    Delete Article function created

--- aller-media/src/components/Articles.tsx
+++ aller-media/src/components/Articles.tsx
@@ -1,5 +1,6 @@
 import { ArticleComponent, ArticleProps } from "./Article"
 import '../styles/Home.css'
+import { useState, useEffect } from "react";
 interface ArticlesProps {
   articles: ArticleProps[]
 }
@@ -8,15 +9,41 @@ const Articles = (props: ArticlesProps) => {
 
   const { articles } = props;
 
+  const [allData, setAllData] = useState<any>()
+
+  useEffect(() => {
+    setAllData(articles)
+  }, [articles])
+
+  const deleteAticle = (title: string) => {
+    setAllData(allData.filter((article: any) => article.title !== title))
+  }
+
+  const UpdateArticle = (title: string, newTitle: string) => {
+
+    let updatedData = allData.map((article: any) => {
+
+      if (article.title === title) {
+        return { ...article, title: newTitle }
+      }
+
+      return article;
+    })
+    setAllData(updatedData)
+
+  }
+
+
   return (
     <div className='home_container'>
       {
-        articles.map((artcl: ArticleProps) => {
+        allData?.map((artcl: ArticleProps) => {
           return <ArticleComponent
             key={artcl.title}
             title={artcl.title}
             url={artcl.url}
             imageUrl={artcl.imageUrl}
+            deleteArticle={deleteAticle}
           />
         })
       }
--- aller-media/src/components/article.tsx
+++ aller-media/src/components/article.tsx
@@ -4,12 +4,12 @@ export interface ArticleProps {
   imageUrl: string,
   title: string,
   url: string,
-
+  deleteArticle(title: string): any
 }
 
 export const ArticleComponent = (props: ArticleProps) => {
 
-  const { imageUrl, title, url } = props;
+  const { imageUrl, title, url, deleteArticle } = props;
 
   const [enableUpdate, setEnableUpdate] = useState<boolean>(false)
 
@@ -33,7 +33,7 @@ export const ArticleComponent = (props: ArticleProps) => {
             <div className='update_section'>
               <h1 className='title'>{title}</h1>
               <div className='update_actions'>
-                <button className='btn delete' type='button'>Delete</button>
+                <button className='btn delete' type='button' onClick={() => deleteArticle(title)}>Delete</button>
                 <button className='btn update' type='button' onClick={() => setEnableUpdate(true)}>Update</button>
               </div>
             </div>
