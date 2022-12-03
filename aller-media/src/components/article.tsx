488ab110ac2819abc20ee8d7bf9ccf832432c956
ValentinVasilev <falioVasilev@gmail.com>
2022-12-03 18:10:18 +0200

    Delete Notification created.

--- aller-media/src/components/article.tsx
+++ aller-media/src/components/article.tsx
@@ -13,34 +13,52 @@ export const ArticleComponent = (props: ArticleProps) => {
   const { imageUrl, title, url, deleteArticle, updateArticle } = props;
 
   const [enableUpdate, setEnableUpdate] = useState<boolean>(false)
+  const [deleteNotification, setDeleteNotification] = useState<boolean>(false)
   const [updateArticleTitle, setUpdateArticleTitle] = useState<string>('')
 
   return (
     <article className="article_container">
-      <a target="_blank" rel="noreferrer" href={url} className='link'>
-        <img src={imageUrl} alt={imageUrl} className='img_container' />
-      </a>
       {
-        enableUpdate
-          ? (
-            <div className='update_section'>
-              <input placeholder={title} autoFocus onChange={(e: ChangeEvent<HTMLInputElement>) => setUpdateArticleTitle(e.target.value)} />
-              <div className='update_actions'>
-                <button className='btn cancel' type='button' onClick={() => setEnableUpdate(false)}>Cancel</button>
-                <button className='btn save' type='button' onClick={() => updateArticle(title, updateArticleTitle)}>Save</button>
-              </div>
+        deleteNotification
+          ? (<div className='delete_notification'>
+            <h1>Are you sure you want to delete this article?</h1>
+            <div className='delete_options'>
+              <button className='btn delete' type='button' onClick={() => deleteArticle(title)}>Delete</button>
+              <button className='btn cancel' type='button' onClick={() => setDeleteNotification(false)}>Cancel</button>
             </div>
+
+          </div>
           )
           : (
-            <div className='update_section'>
-              <h1 className='title'>{title}</h1>
-              <div className='update_actions'>
-                <button className='btn delete' type='button' onClick={() => deleteArticle(title)}>Delete</button>
-                <button className='btn update' type='button' onClick={() => setEnableUpdate(true)}>Update</button>
-              </div>
+            <div className='article_card'>
+              <a target="_blank" rel="noreferrer" href={url} className='link'>
+                <img src={imageUrl} alt={imageUrl} className='img_container' />
+              </a>
+              {
+                enableUpdate
+                  ? (
+                    <div className='update_section'>
+                      <input className='article_input' placeholder={title} autoFocus onChange={(e: ChangeEvent<HTMLInputElement>) => setUpdateArticleTitle(e.target.value)} />
+                      <div className='update_actions'>
+                        <button className='btn cancel' type='button' onClick={() => setEnableUpdate(false)}>Cancel</button>
+                        <button className='btn save' type='button' onClick={() => updateArticle(title, updateArticleTitle)}>Save</button>
+                      </div>
+                    </div>
+                  )
+                  : (
+                    <div className='update_section'>
+                      <h1 className='title'>{title}</h1>
+                      <div className='update_actions'>
+                        <button className='btn delete' type='button' onClick={() => setDeleteNotification(true)}>Delete</button>
+                        <button className='btn update' type='button' onClick={() => setEnableUpdate(true)}>Update</button>
+                      </div>
+                    </div>
+                  )
+              }
             </div>
           )
       }
+
     </article>
   )
 }
